77bae94575c130220980f7d3426416b4fed95cb1
sultangit2000 <tassboke@gmail.com>
2023-02-22 00:53:16 +0600

    'hw'

---
+++ git.py
@@ -0,0 +1,3 @@
+file = open('git_code.txt','w')
+file.write("git init -r git remote add origin link,create .gitignore,.idea,.gitignore,__pycache__/,"
+            "venv,git add .,git commit -m '',git push origin master,git status -s,git rm --cached,git rm --cached")
